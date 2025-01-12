abf9abae3aa72724fb0b4157b30fa4f60be70e56
Yash1234567-spec <realtymedia93@gmail.com>
2025-01-12 22:57:58 +0500

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,55 @@
+
+
+<html lang="en">
+<head>
+    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NLWHVGRS');</script>
+    <!-- End Google Tag Manager -->
+
+ 
+
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Oasis Venetia Heights</title>
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700' rel='stylesheet' type='text/css'>
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+
+
+
+
+
+</head>
+<body>
+
+	<header class="site-header" id="header">
+	<h5 style="font-size: 12px;" class="site-header__title" data-lead-id="site-header-title">THANK YOU WE WILL CONTACT YOU SOON !<br>OR CALL <a href="tel:+918929008045"> 
+	8929008045</a> 
+</h5> 
+	</header>
+
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
+	
+	</div>
+
+	<footer class="site-footer" id="footer">
+		<p class="site-footer__fineprint" id="fineprint">Copyright ©2023 | All Rights Reserved <a href = "https://venetia-heights.pcmapl.in/">8929008045</p>
+	</footer>
+</head>
+<body class="html front not-logged-in one-sidebar sidebar-first page-node" >
+
+
+</body>
+</html>
